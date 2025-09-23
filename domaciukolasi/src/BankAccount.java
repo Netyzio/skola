f2703b240dec51ef24c30ef791bb06d01eb9539e
Netyzio <112963436+Netyzio@users.noreply.github.com>
2025-09-23 19:39:46 +0200

    Update BankAccount.java

--- domaciukolasi/src/BankAccount.java
+++ domaciukolasi/src/BankAccount.java
@@ -1,16 +1,27 @@
 public class BankAccount {
-    private String accountNumber;
-    private double balance;
-    private User firstName;
+    private final double balance;
+    private final User fullName;
+    private final Bank bankName;
 
-    public BankAccount(String accountNumber, double balance, User firstName) {
-this.accountNumber = accountNumber;
-this.balance = balance;
-this.firstName = firstName;
+    public BankAccount(double balance, User fullName, Bank bankName) {
+        this.fullName = fullName;
+        this.balance = balance;
+        this.bankName = bankName;
+        bankName.addAccoount(this);
+        bankName.addFullName(this);
     }
 
-    public boolean isInDebt() {
 
+    public boolean isInDebt() {
+        return balance < 0;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public User getFullName() {
+        return fullName;
     }
 
 }
