cb52bc0fd5de88f32bc1aca89853a9ada3ecfe68
Netyzio <112963436+Netyzio@users.noreply.github.com>
2025-09-23 19:39:23 +0200

    Update Bank.java

--- domaciukolasi/src/Bank.java
+++ domaciukolasi/src/Bank.java
@@ -1,14 +1,45 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Bank {
-    private String code;
-    private String name;
-public Bank(String code, String name) {
-    this.code = code;
-    this.name = name;
-}
 
-public int getTotalBalance() {
+    private final String name;
+    private final ArrayList<BankAccount> accounts = new ArrayList<BankAccount>();
+    private final ArrayList<BankAccount> fullNames = new ArrayList<BankAccount>();
 
-}
-public int getUserCount() {}
+    public Bank(String name) {
+
+        this.name = name;
+    }
+
+    public void addAccoount(BankAccount account) {
+        accounts.add(account);
+    }
+
+    public void addFullName(BankAccount fullName) {
+        fullNames.add(fullName);
+    }
+
+
+    public int getTotalBalance() {
+        int total = 0;
+        for (int i = 0; i < accounts.size(); i++) {
+            BankAccount account = accounts.get(i);
+            total += (int) account.getBalance();
+        }
+        return total;
+    }
+
+    public int getUserCount() {
+        Set<User> uniqueUsers = new HashSet<>();
+
+        for (int i = 0; i < accounts.size(); i++) {
+            uniqueUsers.add(accounts.get(i).getFullName());
+        }
+
+        return uniqueUsers.size();
+    }
+
 
 }
